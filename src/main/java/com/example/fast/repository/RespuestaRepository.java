dd920b12cc2729b984836d80741871e52b0360c4
GeralRosas20 <rosasgeraldine0@gmail.com>
2022-11-18 19:42:40 -0600

    Crud tabla respuesta

---
+++ src/main/java/com/example/fast/repository/RespuestaRepository.java
@@ -0,0 +1,7 @@
+package com.example.fast.repository;
+
+import com.example.fast.model.Respuesta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RespuestaRepository extends JpaRepository<Respuesta, Long> {
+}
